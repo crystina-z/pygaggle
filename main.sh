b54c17f843000ddc59c8045617d750c6233f31d8
Crystina <x978zhan@uwaterloo.ca>
2021-02-17 17:21:49 -0500

    clean up

--- main.sh
+++ main.sh
@@ -1,15 +1,20 @@
 # aggregate the data preprocess, train and eval
 
-export RUN_NAME=resampled_frombm25_shuffled
+# to change each time
+export RUN_NAME=resample.bm25.shuffled.2
+export TSV_NAME=triple.small.train.shuffled.txt
+
+# globals
 export GS_FOLDER=gs://crys-west4-a/t5/${RUN_NAME}
-DATA_DIR=/home/crystina/src/sigir2021/pygaggle/data/msmarco_passage/${RUN_NAME}
+export DATA_DIR=data/msmarco_passage/train_set/${RUN_NAME}
+export RESULT_DIR=data/msmarco_passage/results/${RUN_NAME}
 
 #######################
 # prepare data
 #######################
 export OUTP_DATA_FN=${DATA_DIR}/query_doc_pairs.train.shuffled.tsv
 python pygaggle/data/create_msmarco_t5_training_pairs.py \
-  --triples_train ${DATA_DIR}/triples.train.small.id.shuffled.tsv \
+  --triples_train ${DATA_DIR}/${TSV_NAME} \
   --output_to_t5 ${OUTP_DATA_FN}
 gsutil cp ${OUTP_DATA_FN} ${GS_FOLDER}/
 
@@ -20,13 +25,12 @@ gsutil cp ${OUTP_DATA_FN} ${GS_FOLDER}/
 export MODEL_NAME=base
 export PROJECT_NAME=crystina-project
 export TPU_NAME=crys-1
-
 export MODEL_INIT_CKPT=999900
+
 # Copy pre-trained checkpoint to our target model
 echo "model_checkpoint_path: \"model.ckpt-${MODEL_INIT_CKPT}\"" > checkpoint
 gsutil cp checkpoint ${GS_FOLDER}
 gsutil cp gs://t5-data/pretrained_models/${MODEL_NAME}/model.ckpt-${MODEL_INIT_CKPT}* ${GS_FOLDER}
-
 
 t5_mesh_transformer  \
   --tpu="${TPU_NAME}" \
@@ -51,7 +55,6 @@ t5_mesh_transformer  \
 #######################
 # inference
 #######################
-export MODEL_DIR=${GS_FOLDER}
 export DEV_DATA_FOLDER=gs://crys-west4-a/t5/data/eval_data
 
 for ITER in 000 001 002 003 004 005 006 007 008
@@ -61,7 +64,7 @@ do
     --tpu="${TPU_NAME}" \
     --gcp_project="${PROJECT_NAME}" \
     --tpu_zone="europe-west4-a" \
-    --model_dir="${MODEL_DIR}" \
+    --model_dir="${GS_FOLDER}" \
     --gin_file="gs://t5-data/pretrained_models/${MODEL_NAME}/operative_config.gin" \
     --gin_file="infer.gin" \
     --gin_file="beam_search.gin" \
@@ -78,14 +81,13 @@ do
 done
 
 
-
-export DATA_DIR=data/msmarco_passage
-export RESULT_DIR=data/msmarco_passage/results/${RUN_NAME}
+#######################
+# evaluate
+#######################
 mkdir -p $RESULT_DIR
 
 gsutil cp ${GS_FOLDER}/query_doc_pair_scores.dev.small.txt*-1100000 ${RESULT_DIR}/
 cat ${RESULT_DIR}/query_doc_pair_scores.dev.small.txt*-1100000 > ${RESULT_DIR}/query_doc_pair_scores.dev.small.txt
-
 
 python pygaggle/data/convert_monot5_output_to_msmarco_run.py \
         --t5_output ${RESULT_DIR}/query_doc_pair_scores.dev.small.txt \
